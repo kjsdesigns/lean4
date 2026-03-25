74f74acd377d7324370291d177d7ca6893c13571
Joachim Breitner <mail@joachim-breitner.de>
2026-03-25 10:50:13 +0000

    Revert "chore: disable `#print axioms` under the module system (#8174)"
    This reverts commit 4c497eaa32be75b4df1fb7affcf4036984dd901e.

--- src/Lean/Util/CollectAxioms.lean
+++ src/Lean/Util/CollectAxioms.lean
@@ -7,10 +7,13 @@ module
 
 prelude
 public import Lean.MonadEnv
+import Lean.CoreM
 
 public section
 
 namespace Lean
+
+private builtin_initialize exportedAxiomsExt : MapDeclarationExtension (Array Name) ← mkMapDeclarationExtension
 
 namespace CollectAxioms
 
@@ -20,12 +23,19 @@ structure State where
 
 abbrev M := ReaderT Environment $ StateM State
 
-partial def collect (c : Name) : M Unit := do
+private partial def collect (c : Name) : M Unit := do
   let collectExpr (e : Expr) : M Unit := e.getUsedConstants.forM collect
   let s ← get
   unless s.visited.contains c do
     modify fun s => { s with visited := s.visited.insert c }
     let env ← read
+    if let some axioms := exportedAxiomsExt.find? env c then
+      for ax in axioms do
+        if ax == c then
+          modify fun s => { s with axioms := s.axioms.push c }
+        else
+          collect ax
+      return
     -- We should take the constant from the kernel env, which may differ from the one in the elab
     -- env in case of (async) errors.
     match env.checked.get.find? c with
@@ -47,5 +57,13 @@ def collectAxioms [Monad m] [MonadEnv m] (constName : Name) : m (Array Name) := 
   let env ← getEnv
   let (_, s) := ((CollectAxioms.collect constName).run env).run {}
   pure s.axioms
+
+/--
+Registers axioms used by the declaration in an environment extension so they can be retrieved even
+if some parts of the declaration's become inaccessible.
+-/
+def registerAxiomsForDecl (n : Name) : CoreM Unit := do
+  let axioms ← collectAxioms n
+  modifyEnv (exportedAxiomsExt.addEntry · (n, axioms))
 
 end Lean
